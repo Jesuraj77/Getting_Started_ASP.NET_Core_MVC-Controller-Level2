f4aac6d76393a8d81fbce1bbb54bcefa51b7723a
Jesuraj77 <isysraju2001@gmail.com>
2020-03-23 15:53:13 +0530

    Code Added

---
+++ Level2/Controllers/AboutUsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level2.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        //implement the code here
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+
+    }
+}
